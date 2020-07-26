5c28c1b951488e93153412b946a06ca7e60bd876
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-25 17:36:40 -0700

    Prettier

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,15 @@ const IndexPage = () => (
   <div>
     <h1>Hello Next.js 👋</h1>
     <div className="pure-g">
-        <div className="pure-u-1-3"><p>Thirds</p></div>
-        <div className="pure-u-1-3"><p>Thirds</p></div>
-        <div className="pure-u-1-3"><p>Thirds</p></div>
+      <div className="pure-u-1-3">
+        <p>Thirds</p>
+      </div>
+      <div className="pure-u-1-3">
+        <p>Thirds</p>
+      </div>
+      <div className="pure-u-1-3">
+        <p>Thirds</p>
+      </div>
     </div>
   </div>
 )
