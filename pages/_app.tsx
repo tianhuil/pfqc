b1d573c26f8f7f176078f7c18b3d9a99a0b3adf7
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-26 00:54:28 -0700

    Basic styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,18 @@
 import 'purecss'
 import 'normalize.css'
 import '../public/styles.css'
+import { ThemeProvider } from 'styled-components'
+
+const theme = {
+  colors: {
+    primary: '#0070f3',
+  },
+}
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
 }
