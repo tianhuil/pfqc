7a003233991a9bf48a1d414acfeecc9ff0787a4a
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-25 17:32:02 -0700

    normalize.css

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,5 +1,6 @@
 import 'purecss'
+import 'normalize.css'
 
 export default function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />
+  return <Component {...pageProps} />
 }
