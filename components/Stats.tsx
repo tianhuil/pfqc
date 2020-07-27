f00014098d9ef0697c7567a9352c15694e22da3f
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-27 01:02:08 -0700

    Set layout

--- components/Stats.tsx
+++ components/Stats.tsx
@@ -7,14 +7,21 @@ const Number = styled.span`
   font-family: 'Poppins';
   font-style: normal;
   font-weight: bold;
-  margin: ${(p) => p.theme.lineHeight(1)} 12px;
   color: ${(p) => p.theme.color.orange};
   font-size: 24px;
-  line-height: 32px;
+  line-height: 24px;
+  margin: 24px 0 0 0;
 
   ${(p) => p.theme.mediaQuery.tablet} {
+    font-size: 32px;
+    line-height: 32px;
+    margin: 12px 8px;
+  }
+
+  ${(p) => p.theme.mediaQuery.desktop} {
     font-size: ${(p) => p.theme.lineHeight(2)};
     line-height: ${(p) => p.theme.lineHeight(3)};
+    margin: ${(p) => p.theme.lineHeight(1)} 12px;
   }
 `
 
@@ -45,7 +52,7 @@ const Stat: React.FC<StatProp> = ({ number, unit }) => {
 export const Stats = () => {
   return (
     <Section marginY={0}>
-      <Flex container justifyContent="center">
+      <Flex container justifyContent="space-around">
         <Stat number="10th" unit="Annual" />
         <Stat number="240+" unit="Speakers" />
         <Stat number="5K+" unit="Attendees" />
