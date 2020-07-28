788551baba35062db544cf948d4bb6a5cd5f90e9
Lukas Danin <lukas.danin@gmail.com>
2020-07-27 22:33:53 -0300

    Fixes content not using all width on mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,7 +1,6 @@
 import styled from 'styled-components'
 
 export const NavbarLayout = styled.div`
-  position: absolute;
   width: 100%;
   z-index: 2;
   height: ${(p) => p.theme.lineHeight(3)};
@@ -10,20 +9,16 @@ export const NavbarLayout = styled.div`
 
 export const SplashLayout = styled.div`
   background: #ccc;
-  z-index: 1;
   overflow: hidden;
   width: calc(100vw - (100vw - 100%));
-  height: 88%;
+  height: 88vh;
   top: 0;
   left: 0;
-  position: absolute;
+  position: relative;
 `
 
 export const BodyLayout = styled.div`
-  position: absolute;
-  top: 87%;
   min-height: 13%;
   width: 100%;
-  z-index: 2;
   background: white;
 `
