7efc3f87a38979e2c9ddac8340ae51834729aa41
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-29 10:36:09 -0700

    Adjusting layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,16 +9,17 @@ export const NavbarLayout = styled.div`
 
 export const SplashLayout = styled.div`
   background: #ccc;
+  z-index: 1;
   overflow: hidden;
-  width: calc(100vw - (100vw - 100%));
+  width: 100%;
   height: 88vh;
   top: 0;
   left: 0;
-  position: relative;
 `
 
 export const BodyLayout = styled.div`
-  min-height: 13%;
+  min-height: 13vh;
+  z-index: 2;
   width: 100%;
   background: white;
 `
