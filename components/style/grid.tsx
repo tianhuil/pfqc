04c18b29eb99757a0bbf319eae1a271028517f98
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2020-07-30 18:33:44 -0700

    tmp

--- components/style/grid.tsx
+++ components/style/grid.tsx
@@ -6,26 +6,36 @@ export const Row = styled.div`
   margin: 0 -20px;
 `
 
-interface ColProps {
-  before?: number
+interface ColSpacing {
+  left?: number
   size?: number
-  after?: number
+  right?: number
+  padding?: string
 }
 
-const RawCol = styled.div<ColProps>`
-  flex: ${(p) => p.size ?? 1};
-  padding: 0 20px;
-`
+interface ColProps extends ColSpacing {
+  tablet?: ColSpacing
+  desktop?: ColSpacing
+}
 
 /**
- * A column that has before empty column and after empty column
+ * A 4-col column layout
  */
-export const Col: React.FC<ColProps> = ({ before, size, after, children }) => {
-  return (
-    <>
-      {before ? <RawCol size={before} /> : null}
-      <RawCol size={size}>{children}</RawCol>
-      {after ? <RawCol size={after} /> : null}
-    </>
-  )
+export const Col: React.FC<ColProps> = (props) => {
+  const properties = ({ left, size, right, padding }: ColSpacing) => `
+    margin: 0 ${right ? right * 25 : 0}% 0 ${left ? left * 25 : 0}%;
+    width: ${size * 25}%;
+    padding: 0 ${padding ?? '20px'};
+  `
+
+  const Component = styled.div<ColProps>`
+    ${properties(props)}
+
+    ${(p) => p.theme.mediaQuery.tablet} {
+      margin: 0 ${(p) => p.right * 25}% 0 ${(p) => p.left * 25}%;
+      width: ${(p) => p.size * 25}%;
+    }
+  `
+
+  return <Component>{props.children}</Component>
 }
