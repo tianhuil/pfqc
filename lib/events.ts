a5d191fea8b605d9c594732ea02a5388106229d5
mconstan3011 <65615322+mconstan3011@users.noreply.github.com>
2020-08-10 18:40:42 -0400

    updated links in events.ts

--- lib/events.ts
+++ lib/events.ts
@@ -62,67 +62,188 @@ const toEvent = (m: Moment, speakers: [Speaker, Speaker, Speaker]): Event => {
 }
 
 export const getEventProps = (): Event[] => {
-  const m = moment('2020-09-30T16:00:00Z')
+  const m = moment('2020-09-26T16:00:00Z')
 
   return [
-    toEvent(moment('2020-09-30T16:00:00Z'), [
+    toEvent(moment('2020-09-26T16:00:00Z'), [
       {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
+        name: 'Christina Qi',
+        title: 'Co-Founder',
+        company: 'Domeyard',
+        image: 'images/headshot/Qi.png',
       },
       {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
+        name: 'John Birge',
+        title: 'Chief Risk Officer',
+        company: 'Credibly',
+        image: 'images/headshot/Birge.png',
       },
       {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
-      },
-    ]),
-    toEvent(moment('2020-10-30T16:00:00Z'), [
-      {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
-      },
-      {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
-      },
-      {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
+        name: 'Abraham Thomas',
+        title: 'Finance Change and Regulatory Reporting',
+        company: 'Deutsche Bank',
+        image: 'images/headshot/Thomas.png',
       },
     ]),
-    toEvent(moment('2020-11-30T16:00:00Z'), [
+    toEvent(moment('2020-10-09T16:00:00Z'), [
       {
         name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
+        title: 'Head of Quantitative Research',
+        company: 'Bloomberg LP',
         image: 'images/headshot/Dupire.png',
       },
       {
-        name: 'Bruno Dupire',
-        title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
+        name: 'Andrew Peek',
+        title: 'Co-Founder and CEO',
+        company: 'Delphia',
+        image: 'images/headshot/Peek.png',
       },
       {
-        name: 'Bruno Dupire',
+        name: 'Justin Zhen',
+        title: 'Co-Founder ',
+        company: 'Thinknum Alternative Data',
+        image: 'images/headshot/Zhen.png',
+      },
+    ]),
+    toEvent(moment('2020-10-24T16:00:00Z'), [
+      {
+        name: 'Nick Alonso',
+        title: 'TBA',
+        company: 'TBA',
+        image: 'images/headshot/Alonso.png',
+      },
+      {
+        name: 'Chris Bemis',
+        title: 'Head of Quantitative Analysis and Research',
+        company: 'Whitebox Advisors',
+        image: 'images/headshot/Bemis.png',
+      },
+      {
+        name: 'Michael Miller',
+        title: 'CEO',
+        company: 'Northstar Risk Corporation',
+        image: 'images/headshot/Miller.png',
+      },
+    ]),
+    toEvent(moment('2020-11-21T16:00:00Z'), [
+      {
+        name: 'Boris Lerner',
+        title: 'Global Head of Quantitative Equity Research',
+        company: 'Morgan Stanley',
+        image: 'images/headshot/Lerner.png',
+      },
+      {
+        name: 'Ali Hirsa',
+        title: 'Managing Partner',
+        company: 'Sauma Capital',
+        image: 'images/headshot/Hirsa.png',
+      },
+      {
+        name: 'Gurraj Singh Sangha',
+        title: 'Head of Data Science, Risk, and MI',
+        company: 'State Street',
+        image: 'images/headshot/Sangha.png',
+      },
+    ]),
+    toEvent(moment('2020-12-05T16:00:00Z'), [
+      {
+        name: 'Mathew Granade',
+        title: 'Chief Market Intelligence Officer',
+        company: 'Point72',
+        image: 'images/headshot/Granade.png',
+      },
+      {
+        name: 'Yury Blyakhman',
+        title:
+          'Managing Director, Head of Linear Rates and FX Quant Research, Emerging and Developed Markets',
+        company: 'JP Morgan',
+        image: 'images/headshot/Blyakhman.png',
+      },
+      {
+        name: 'Gregory Pelts',
+        title: 'Director',
+        company: 'Scotiabank',
+        image: 'images/headshot/Pelts.png',
+      },
+    ]),
+    toEvent(moment('2020-01-31T16:00:00Z'), [
+      {
+        name: 'Dr. Harvey Stein	',
+        title: 'Head, Quantitative Risk Analytics',
+        company: 'Bloomberg LP',
+        image: 'images/headshot/Stein.png',
+      },
+      {
+        name: 'Sebastian Jaimungal',
+        title: 'Professor',
+        company: 'University of Toronto',
+        image: 'images/headshot/Jaimungal.png',
+      },
+      {
+        name: 'Terry Benzschawel',
+        title: 'Founder and Principal',
+        company: 'Benzschawel Scientific LLC',
+        image: 'images/headshot/Benzschawel.png',
+      },
+    ]),
+    toEvent(moment('2020-02-27T16:00:00Z'), [
+      {
+        name: 'TBA',
+        title: 'TBA',
+        company: 'TBA',
+        image: 'images/headshot/TBA.png',
+      },
+      {
+        name: 'George Mylnikov',
+        title: 'Portfolio Manager, Head of Quantitative Research',
+        company: 'Windhaven Investment Management',
+        image: 'images/headshot/Mylnikov.png',
+      },
+      {
+        name: 'Alexander Fleiss',
+        title: 'CEO',
+        company: 'RebellionResearch',
+        image: 'images/headshot/Fleiss.png',
+      },
+    ]),
+    toEvent(moment('2020-03-27T16:00:00Z'), [
+      {
+        name: 'TBA',
+        title: 'TBA',
+        company: 'TBA',
+        image: 'images/headshot/TBA.png',
+      },
+      {
+        name: 'Jess Stauth',
         title: 'Managing Director',
-        company: 'Bloomberg',
-        image: 'images/headshot/Dupire.png',
+        company: 'Fidelity Labs',
+        image: 'images/headshot/Stauth.png',
+      },
+      {
+        name: 'Alex Izydorcik',
+        title: 'Head of Data Science',
+        company: 'Coatue',
+        image: 'images/headshot/Izydorcik.png',
+      },
+    ]),
+    toEvent(moment('2020-04-24T16:00:00Z'), [
+      {
+        name: 'TBA',
+        title: 'TBA',
+        company: 'TBA',
+        image: 'images/headshot/TBA.png',
+      },
+      {
+        name: 'Dr. Miquel Noguer i Alonso',
+        title: 'Founder',
+        company: 'Artificial Intelligence Finance Institute',
+        image: 'images/headshot/NogueriAlonso.png',
+      },
+      {
+        name: 'Kyle Schlotman',
+        title: 'Partner and CIO',
+        company: 'Connetic Ventures',
+        image: 'images/headshot/Schlotman.png',
       },
     ]),
   ]
