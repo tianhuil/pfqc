adb26f17e5e1cd6d98423aa112e2428eb8732b7c
Karen Zhou <zz2202@nyu.edu>
2020-09-24 20:38:15 -0400

    added all committee members

--- lib/committee.ts
+++ lib/committee.ts
@@ -21,16 +21,10 @@ export const getCommitteeProps = (): Committee[] => {
     return [
         toCommittee("Marketing and Logistics", [
             {
-                name: 'Marius Constantin',
-                title: 'Risk Engineering Strats Analyst',
-                company: 'Goldman Sachs',
-                image: 'images/committee_headshot/Constantin.jpeg',
-            },
-            {
-                name: 'Katelyn Schoenberger',
-                title: 'Software Engineer',
-                company: 'JPMorgan Chase & Co.',
-                image: 'images/committee_headshot/Schoenberger.jpeg',
+                name: 'Claudia Popescu',
+                title: 'Class of 2021',
+                company: 'Princeton University',
+                image: 'images/committee_headshot/Popescu.jpeg',
             },
             {
                 name: 'Karen Zhou',
@@ -39,16 +33,10 @@ export const getCommitteeProps = (): Committee[] => {
                 image: 'images/committee_headshot/Zhou.jpeg',
             },
             {
-                name: 'Lucy Lu',
-                title: 'Class of 2021',
-                company: 'Cornell University',
-                image: 'images/committee_headshot/Lu.jpeg',
-            },
-            {
-                name: 'Soha Aslam',
-                title: 'Class of 2020',
-                company: 'Princeton University',
-                image: 'images/committee_headshot/Aslam.jpeg',
+                name: 'Katelyn Schoenberger',
+                title: 'Software Engineer',
+                company: 'JPMorgan Chase & Co.',
+                image: 'images/committee_headshot/Schoenberger.jpeg',
             },
             {
                 name: 'Kristian Bjarnason',
@@ -57,24 +45,36 @@ export const getCommitteeProps = (): Committee[] => {
                 image: 'images/committee_headshot/Bjarnason.jpeg',
             },
             {
-                name: 'Claudia Popescu',
+                name: 'Lucy Lu',
                 title: 'Class of 2021',
+                company: 'Cornell University',
+                image: 'images/committee_headshot/Lu.jpeg',
+            },
+            {
+                name: 'Marius Constantin',
+                title: 'Risk Engineering Strats Analyst',
+                company: 'Goldman Sachs',
+                image: 'images/committee_headshot/Constantin.jpeg',
+            },
+            {
+                name: 'Soha Aslam',
+                title: 'Class of 2020',
                 company: 'Princeton University',
-                image: 'images/committee_headshot/Popescu.jpeg',
+                image: 'images/committee_headshot/Aslam.jpeg',
             },
         ]),
         toCommittee("Speakers", [
             {
-                name: 'Nicholas Chow',
-                title: 'Class of 2018',
-                company: 'Princeton University',
-                image: 'images/committee_headshot/Chow.jpeg',
+                name: 'Binayak Kandel',
+                title: 'Class of 2019',
+                company: 'Boston University',
+                image: 'images/committee_headshot/Kandel.jpeg',
             },
             {
-                name: 'Paul Krueger',
-                title: 'Class of 2021',
+                name: 'Christy Matthews',
+                title: 'Class of 2020',
                 company: 'Princeton University',
-                image: 'images/committee_headshot/Krueger.jpeg',
+                image: 'images/committee_headshot/Matthews.jpeg',
             },
             {
                 name: 'Greeshma Balabhadra',
@@ -83,10 +83,46 @@ export const getCommitteeProps = (): Committee[] => {
                 image: 'images/committee_headshot/Balabhadra.jpeg',
             },
             {
-                name: 'Binayak Kandel',
+                name: 'Michael Li',
+                title: 'Founder and President',
+                company: 'The Data Incubator',
+                image: 'images/committee_headshot/Li.jpeg',
+            },
+            {
+                name: 'Nicholas Chow',
+                title: 'Class of 2018',
+                company: 'Princeton University',
+                image: 'images/committee_headshot/Chow.jpeg',
+            },
+            {
+                name: 'Sarah Nagy',
+                title: 'Sector Data Analyst',
+                company: 'Ashler Capital',
+                image: 'images/committee_headshot/Nagy.jpeg',
+            },
+            {
+                name: 'Maria Velicu',
+                title: 'Class of 2020',
+                company: 'Yale University',
+                image: 'images/committee_headshot/Velicu.jpeg',
+            },
+            {
+                name: 'Paul Krueger',
+                title: 'Class of 2021',
+                company: 'Princeton University',
+                image: 'images/committee_headshot/Krueger.jpeg',
+            },
+            {
+                name: 'Rui Ding',
+                title: 'Class of 2023',
+                company: 'Stony Brook University',
+                image: 'images/committee_headshot/Ding.jpeg',
+            },
+            {
+                name: 'Ruiqing Cao',
                 title: 'Class of 2019',
-                company: 'Boston University',
-                image: 'images/committee_headshot/Kandel.jpeg',
+                company: 'Harvard University',
+                image: 'images/committee_headshot/Cao.jpeg',
             },
         ]),
     ]
